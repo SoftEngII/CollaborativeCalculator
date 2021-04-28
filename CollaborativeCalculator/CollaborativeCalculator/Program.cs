55ef59d9fbd96fb8dbb424e214e95544d42ef093
Matt <matthewtmuir@gmail.com>
2021-04-28 08:35:06 -0700

    Add VS Project Files

---
+++ CollaborativeCalculator/CollaborativeCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollaborativeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
