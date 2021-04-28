30c7c2438db8b976ad267069e67b724f463e1eea
Matt <matthewtmuir@gmail.com>
2021-04-28 08:44:25 -0700

    Add add subtract to Calc class

--- CollaborativeCalculator/CollaborativeCalculator/Calculator.cs
+++ CollaborativeCalculator/CollaborativeCalculator/Calculator.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CollaborativeCalculator
 {
-    class Calculator
+    public class Calculator
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
